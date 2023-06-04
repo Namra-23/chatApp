d0c10090138c0b8b434f8d0b65f04b8e972329c8
Namra-23 <namrajoshi2303@gmail.com>
2023-06-05 00:02:18 +0530

    tailwind

--- chatapp/views.py
+++ chatapp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render,redirect
-from .models import ChatRoom
+from django.shortcuts import render, redirect
+from .models import ChatRoom, ChatMessage
 
 # Create your views here.
+
+
 def index(request):
     chatrooms = ChatRoom.objects.all()
-    return render(request, 'chatapp/index.html',{'chatrooms' : chatrooms})
+    return render(request, 'chatapp/index.html', {'chatrooms': chatrooms})
 
-def chatroom(request,slug):
+
+def chatroom(request, slug):
     chatroom = ChatRoom.objects.get(slug=slug)
-    return render(request,'chatapp/room.html',{'chatroom' : chatroom})
+    messages = ChatMessage.objects.filter(room=chatroom)[0:30]
+    return render(request, 'chatapp/room.html', {'chatroom': chatroom,'messages':messages})
